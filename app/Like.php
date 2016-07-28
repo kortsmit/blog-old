11d023b28943b3e4009f61eac1aac274bf10f80c
Tim Kortsmit <tim@kortsmit.com>
2016-07-27 21:47:42 -0400

    Models

---
+++ app/Like.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Blog;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Like extends Model
+{
+    protected $table = 'likes';
+    
+}
---
+++ app/Photo.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Blog;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Photo extends Model
+{
+    protected $table = 'photos';
+}
