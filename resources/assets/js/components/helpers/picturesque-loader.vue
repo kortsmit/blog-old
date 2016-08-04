4e2093d1d89f81ee4a6ab49c3d7bf30d145a0cdf
Tim Kortsmit <tim@kortsmit.com>
2016-08-03 22:20:21 -0400

    More component renaming

--- resources/assets/js/components/helpers/picturesque-loader.vue
+++ resources/assets/js/components/helpers/picturesque-loader.vue
@@ -6,7 +6,7 @@
 
 <script>
    export default {
-        name: 'one-loader',
+        name: 'picturesque-loader',
 
         props: {
             loading: false
--- resources/assets/js/components/helpers/picturesque-pagination.vue
+++ resources/assets/js/components/helpers/picturesque-pagination.vue
@@ -43,7 +43,7 @@
 
 <script>
    export default {
-        name: 'one-pagination',
+        name: 'picturesque-pagination',
 
         props: {
             pagination: {
